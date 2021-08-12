4912f6c73e90768d2fe16c47f9b7ee744b8616a5
hzc168 <1048224980@qq.com>
2021-08-12 22:18:46 +0800

    对koa的简单理解

---
+++ 01/koa/lib/application.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+class Application {
+    listen(...args) {
+        const server = http.createServer((req, res) => {
+            res.end('my koa')
+        })
+        server.listen(...args)
+    }
+}
+
+module.exports = Application
